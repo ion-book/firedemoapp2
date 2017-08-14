0515310c3b53fbaa35c457c52c503564509fc57e
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-14 14:02:08 -0500

    terminando el formulario para enviar los archivos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { environment } from '../environments/environment';
 import { LoadingComponent } from './loading/loading.component';
 import { UploadsListComponent } from './uploads-list/uploads-list.component';
 
 import { UploadService } from "./upload.service";
 import { UploadDetailComponent } from './upload-detail/upload-detail.component';
+import { UploadFormComponent } from './upload-form/upload-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoadingComponent,
     UploadsListComponent,
-    UploadDetailComponent
+    UploadDetailComponent,
+    UploadFormComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig)
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule
   ],
   providers: [UploadService],
   bootstrap: [AppComponent]
