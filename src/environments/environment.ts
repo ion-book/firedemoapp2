7d8de46adadc51ae3092584db0b30ea09c1fc8d3
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-14 11:10:04 -0500

    conectando Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,11 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAbD19sI5Xz_jKjEMCQhjuYXVPrdn3YEsY",
+    authDomain: "codelab-22299.firebaseapp.com",
+    databaseURL: "https://codelab-22299.firebaseio.com",
+    projectId: "codelab-22299",
+    storageBucket: "codelab-22299.appspot.com",
+    messagingSenderId: "141181591031"
+  }
 };
