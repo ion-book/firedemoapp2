7e5309c3f8c4009407b669ca2740c063ec84d938
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-08-14 13:23:02 -0500

    creamos upload list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,21 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from '../environments/environment';
 import { LoadingComponent } from './loading/loading.component';
+import { UploadsListComponent } from './uploads-list/uploads-list.component';
+
+import { UploadService } from "./upload.service";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingComponent
+    LoadingComponent,
+    UploadsListComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [],
+  providers: [UploadService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
